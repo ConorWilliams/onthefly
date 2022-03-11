0b27c00a119fe13732f51ab5709454bdbf04b715
ConorWilliams <conorwilliams@outlook.com>
2022-03-11 12:03:23 +0000

    prepping

--- documentation/conf.py
+++ documentation/conf.py
@@ -10,10 +10,10 @@ LINKS_NAVBAR1 = [
 
 # LINKS_NAVBAR1 = [
 #     (None, 'pages', [(None, 'about')]),
-#     (None, 'namespaces', [(None, 'namespacegreeter')]),
+#     (None, 'namespaces', [(None, 'namespaceonthefly')]),
 # ]
 #
 # LINKS_NAVBAR2 = [
-#     (None, 'annotated', [(None, 'classgreeter_1_1_greeter')]),
-#     (None, 'files', [(None, 'greeter_8h')]),
+#     (None, 'annotated', [(None, 'classonthefly_1_1_onthefly')]),
+#     (None, 'files', [(None, 'onthefly_8h')]),
 # ]
---
+++ test/source/onthefly.cpp
@@ -0,0 +1,22 @@
+#include "onthefly/onthefly.h"
+
+#include <string>
+
+#include "doctest/doctest.h"
+#include "onthefly/version.h"
+
+TEST_CASE("OnTheFly") {
+  using namespace onthefly;
+
+  OnTheFly onthefly("Tests");
+
+  CHECK(onthefly.greet(LanguageCode::EN) == "Hello, Tests!");
+  CHECK(onthefly.greet(LanguageCode::DE) == "Hallo Tests!");
+  CHECK(onthefly.greet(LanguageCode::ES) == "¡Hola Tests!");
+  CHECK(onthefly.greet(LanguageCode::FR) == "Bonjour Tests!");
+}
+
+TEST_CASE("OnTheFly version") {
+  static_assert(std::string_view(ONTHEFLY_VERSION) == std::string_view("1.0"));
+  CHECK(std::string(ONTHEFLY_VERSION) == std::string("1.0"));
+}
