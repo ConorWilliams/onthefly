9efc7141f36377d35aea2badcbbb846c660e7acf
ConorWilliams <conorwilliams@outlook.com>
2022-06-02 16:28:15 +0100

    main test perm

--- standalone/src/main.cpp
+++ standalone/src/main.cpp
@@ -11,6 +11,7 @@
 
 #include "libatom/atom.hpp"
 #include "libatom/env/geometry.hpp"
+#include "libatom/utils.hpp"
 
 using namespace otf;
 
@@ -36,9 +37,21 @@ auto main(int, char**) -> int {
 
   std::cout << P.com() << std::endl;
 
+  env::Geometry Q = P;
+
   ///////////////////////////////
 
-  env::Geometry Q = P;
+  fmt::print("Pre rmsd={}\n", P.rmsd(Q));
+
+  for (auto& elem : P) {
+    elem(Position{}) += 0.01 * Vec3<floating>{dis(gen), dis(gen), dis(gen)};
+  }
+
+  fmt::print("Perturb rmsd={}\n", P.rmsd(Q));
+
+  std::shuffle(std::next(P.begin()), P.end(), gen);
+
+  fmt::print("Shuffle rmsd={}\n", P.rmsd(Q));
 
   Mat3<floating> Rot{
       {std::cos(1.0), -std::sin(1.0), 0},
@@ -49,6 +62,13 @@ auto main(int, char**) -> int {
   for (auto& elem : P) {
     elem(Position{}) = Rot * elem(Position{}).matrix();
   }
+
+  fmt::print("Rot rmsd={}\n", P.rmsd(Q));
+
+  timeit("perm", [&] {
+    auto Pp = P;
+    (void)Pp.permute_onto(Q, 1.5);
+  });
 
   return 0;
 }
